fc839faa4ce9fa8d2196c7ea4a178c8f6d688b49
Denis <butko.denis.95@gmail.com>
2014-05-06 18:42:32 +0300

    Delete c__winginx_home_pichub.local_public_html_app_views_show_index.volt.php

--- public_html/app/cache/c__winginx_home_pichub.local_public_html_app_views_show_index.volt.php
+++
@@ -1,4 +0,0 @@
-<img src="<?php echo $image['path']; ?>"><br>
-Описание: <?php echo $image['opis']; ?><br>
-Дата добавления: <?php echo $image['time']; ?><br>
-Просмотров: <?php echo $image['views']; ?>
